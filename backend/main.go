7e0c0935054c7380bcfc895c77de001d0dbcbfd5
sirensan <hosinahikaru@gmail.com>
2026-03-25 17:27:24 +0900

    微修正

--- backend/main.go
+++ backend/main.go
@@ -16,7 +16,7 @@ type Task struct {
 	Deleted bool   `json:"deleted"`
 }
 
-const dbFileName = "tasks_test.json" // テストと共通のファイル名
+const dbFileName = "tasks.json" // データファイル名
 
 // CORS対応のミドルウェア
 func corsMiddleware(next http.Handler) http.Handler {
